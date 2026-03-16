8d16d098f9d8b3a1f5d8e0c3a986181ac0e91d4e
Mykhailo <chuvachock1521@gmail.com>
2026-03-16 09:02:15 +0200

    Added empty eventListener for block-btn

---
+++ scripts.js
@@ -0,0 +1,6 @@
+const blockBtn = document.querySelector("[block-btn]");
+const focusBtn = document.querySelector("[focus-btn]");
+
+blockBtn.addEventListener("submit", (event) => {
+    event.preventDefault();
+});
