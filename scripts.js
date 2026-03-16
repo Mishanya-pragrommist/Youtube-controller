5e045f797b87506b008fb74be11b0e7f152687db
Mykhailo <chuvachock1521@gmail.com>
2026-03-16 16:17:05 +0200

    Some slight changes, working stuff will be soon

--- scripts.js
+++ scripts.js
@@ -1,6 +1,17 @@
+const radioButtons = document.getElementsByName("preference");
+
+const blockRadio = document.querySelector("[block-ytb-entirely]");
+
 const blockBtn = document.querySelector("[block-btn]");
 const focusBtn = document.querySelector("[focus-btn]");
+
+
+blockRadio.addEventListener("checked", () => {
+    blockBtn.style.width = "100px";
+});
+
 
 blockBtn.addEventListener("submit", (event) => {
     event.preventDefault();
 });
+
